2b3dcb2f4b94e9a7a34af61dd903fcab70733442
cesnik-jure <cesnik.jure@gmail.com>
2012-11-05 02:29:47 +0100

    First working version of Rails_3 repository

---
+++ 02_Ch/hello/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = '9018ddc3921a180368a2d08eb697f682c88a460b514557adad07c0a082211f8c0e05d5934aeae810166333862e7d9e2b221dfea17a63371a9fcab1fcaf97a751'
---
+++ 03_Ch/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '6e86c1bb851b93642f530e184f882f583e5836aaed5b5afe5a39a323961efd57b3c4d853f4f7508766c25fb8dcbd786e5a3d8855c99208e9f661ad5826b4d7b6'
